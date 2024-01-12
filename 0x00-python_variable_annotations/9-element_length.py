23cfe75a929ea947de96287fdcad2e1c009cc19b
jailan-sh <jilan.elsherbiny@gmail.com>
2024-01-12 04:25:21 +0200

    pycod

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,6 +3,6 @@
 from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: Iterable[Sequence] ) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """a type-annotated function"""
     return [(i, len(i)) for i in lst]
