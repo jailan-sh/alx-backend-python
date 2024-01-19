b8a064e4142298c9decdf1e53962a32134d22d45
jailan-sh <jilan.elsherbiny@gmail.com>
2024-01-19 22:53:13 +0200

    1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """returns a random delay between 0 and 10 secondsand eventually """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import random
 import asyncio
-from . import wait_random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """returns a random delay between 0 and 10 secondsand eventually """
-    
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
