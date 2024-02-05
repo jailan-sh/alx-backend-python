80b7d6bc5bd3e617fcd9d6a353e61c6ab510ff7c
jailan-sh <jilan.elsherbiny@gmail.com>
2024-02-06 01:26:41 +0200

    6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -35,3 +35,21 @@ class TestGithubOrgClient(unittest.TestCase):
             test_repo_url = test_org._public_repos_url
             self.assertEqual(test_repo_url, mock_pro.return_value.get('repos_url'))
             mock_pro.assert_called_once()
+
+    @patch('client.get_json', return_value=[{'name': 'jil'},
+                                            {'name': 'fw'},
+                                            {'name': 'sher'}])
+    def test_public_repos(self, mock_repo):
+        """
+        Test GithubOrgClient's public_repos method
+        """
+        with patch.object(GithubOrgClient,
+                          '_public_repos_url',
+                          new_callable=PropertyMock,
+                          return_value="https://api.github.com/") as mock_pro:
+            test_client = GithubOrgClient('jil')
+            test_repo = test_client.public_repos()
+            for idx in range(3):
+                self.assertIn(mock_repo.return_value[idx]['name'], test_repo)
+            mock_repo.assert_called_once()
+            mock_pro.assert_called_once()
