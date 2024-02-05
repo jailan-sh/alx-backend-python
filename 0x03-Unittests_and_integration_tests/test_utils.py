17d3e683086afd374ceccb299dbd9dce308b262b
jailan-sh <jilan.elsherbiny@gmail.com>
2024-02-05 02:37:15 +0200

    up

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+unittest for access_nested_map
+"""
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAcessNestedMap(unittest.TestCase):
+    """ test access_nested_map"""
+    @parameterized.expand([])
+    def test_access_nested_map(self, )
